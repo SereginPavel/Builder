812a15352e7037a53f62cb1716352e5af8b6456a
Pavel <Witu7320@yandex.ru>
2017-10-29 20:38:21 +0300

    Initial commit

---
+++ src/Jenkinsfile-dev.groovy
@@ -0,0 +1,24 @@
+def props = [:]
+def rfcList = []
+def build = load 'lib/OneBuild.groovy'
+
+if(env.AdditionalRFC != null){
+    rfcList = env.AdditionalRFC.tokenize(';')
+    println("rfcLost: " + rfcList)
+}
+
+if (env.JobName != null) props.put('JobName', env.JobName)
+node {
+    timestamps{
+        ansiColor('xterm'){
+            stage('build'){
+                for (def rfc : rfcList){
+                    if (props.JobName){
+                        //build job: "$props.JobName", wait: true
+                        build.run();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/lib/OneBuild.groovy
@@ -0,0 +1,10 @@
+package lib
+
+/**
+ * Created by SerP on 29.10.2017.
+ */
+class OneBuild {
+    public def run(){
+        println 'hello'
+    }
+}
