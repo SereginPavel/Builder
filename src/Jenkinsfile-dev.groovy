6aa96aa62a840ddc1db1e8140d8c1c73dfac4462
Pavel <Witu7320@yandex.ru>
2017-10-30 12:13:11 +0300

    changes

--- src/Jenkinsfile-dev.groovy
+++ src/Jenkinsfile-dev.groovy
@@ -21,7 +21,7 @@ node {
                     if (props.JobName){
                         //build job: "$props.JobName", wait: true
                        // println 'hello'
-                        println build
+                        println build.getName();
                         build.run();
                     }
                 }
