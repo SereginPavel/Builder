ba581f128007a0b26d8af7f6035beceef6760f74
Pavel <Witu7320@yandex.ru>
2017-10-30 09:53:56 +0300

    changes

--- src/Jenkinsfile-dev.groovy
+++ src/Jenkinsfile-dev.groovy
@@ -1,6 +1,6 @@
 def props = [:]
 def rfcList = []
-//def build = load 'src/lib/OneBuild.groovy'
+//
 
 if(env.AdditionalRFC != null){
     rfcList = env.AdditionalRFC.tokenize(';')
@@ -9,6 +9,7 @@ if(env.AdditionalRFC != null){
 
 if (env.JobName != null) props.put('JobName', env.JobName)
 node {
+    def build = load 'src/lib/OneBuild.groovy'
     timestamps{
         ansiColor('xterm'){
             stage('build'){
